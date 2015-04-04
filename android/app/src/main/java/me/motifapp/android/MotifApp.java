6a008c12a619de9667890e1c8aa51fdbe054ca5c
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2015-04-04 04:38:02 -0700

    Hello, android world!

---
+++ android/app/src/main/java/me/motifapp/android/MotifApp.java
@@ -0,0 +1,10 @@
+package me.motifapp.android;
+
+import android.app.Application;
+import android.util.Log;
+
+public class MotifApp extends Application {
+    @Override
+    public void onCreate() {
+    }
+}
