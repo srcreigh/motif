20b988cd9771c63b2cb211ec88a39303ace72096
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2015-04-04 06:07:09 -0700

    Add parse for notifications

--- android/app/src/main/java/me/motifapp/android/MotifApp.java
+++ android/app/src/main/java/me/motifapp/android/MotifApp.java
@@ -3,8 +3,30 @@ package me.motifapp.android;
 import android.app.Application;
 import android.util.Log;
 
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
+
 public class MotifApp extends Application {
     @Override
     public void onCreate() {
+        initParse();
+    }
+
+    private void initParse() {
+        final String APP_ID = getString(R.string.parse_app_id);
+        final String CLIENT_KEY = getString(R.string.parse_client_key);
+        Parse.initialize(this, APP_ID, CLIENT_KEY);
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 }
